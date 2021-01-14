36300b1921f912d7666bf80faffce82ef8aff59b
sadaakisz <hssm2000.sadaaki@gmail.com>
2021-01-14 16:13:31 -0500

    Fixed indent and layout of some objects.

--- equilibrium-frontend/src/views/SignUp.vue
+++ equilibrium-frontend/src/views/SignUp.vue
@@ -1,162 +1,167 @@
 <template>
-    <div class="SignUp vh-100 vw-100" style="align-content: center">
-        <b-container align="center">
-            <b-row no-gutters class="back" align-v="center">
-                <b-col>
-                    <img src="../assets/SignUpBG.png" style="width: 28.5vw; height: auto;">
-                </b-col>
-                <b-col>
-                    <router-link to="/">
-                        <img class="close-x" src="../assets/CloseX.png">
-                    </router-link>
-                    <form class="login-form">
-                        <h1 class="registrate-header">Regístrate en Equilibrium</h1>
-                        <h3 class="text">¿Ya tienes cuenta? <a class="form-link" href="/login" rel="noopener">Inicia sesión aquí</a></h3>
-                        <div class="form-text">
-                            <label for="InputUsername">Username</label>
-                            <input type="text" class="form-control" required id="InputUsername">
-                        </div>
-                        <div class="form-text">
-                            <label for="InputEmail">Correo electrónico </label>
-                            <input type="email" class="form-control" required id="InputEmail" placeholder="correo@ejemplo.com">
-                        </div>
-                        <div class="form-text">
-                            <label for="InputPassword">Contraseña</label>
-                            <input type="password" class="form-control" required id="InputPassword">
-                        </div>
-                        <div class="form-text">
-                            <label for="InputConfirmPassword">Confirme la contraseña</label>
-                            <input type="password" class="form-control" required id="InputConfirmPassword">
-                        </div>
-                        <div class="form-text">
-                            <label for="FechaNac">Fecha de Nacimiento</label>
-                            <input class="form-control" required style="alignment: left" id="FechaNac" type="date" name="FechaNac"/>
-                        </div>
-
-                    </form>
-                    <b-button class="register-btn" text to="/sign-up-complete"><p class="text">REGISTRARSE</p></b-button>
-                </b-col>
-            </b-row>
-        </b-container>
-    </div>
+  <div class="SignUp vh-100 vw-100" style="align-content: center">
+    <b-container align="center">
+      <b-row no-gutters class="back" align-v="center">
+        <b-col>
+          <img src="../assets/SignUpBG.png" style="width: 28.5vw; height: auto;">
+        </b-col>
+        <b-col>
+          <router-link to="/">
+            <img class="close-x" src="../assets/CloseX.png">
+          </router-link>
+          <form class="login-form">
+            <h1 class="registrate-header">Regístrate en Equilibrium</h1>
+            <h3 class="text">¿Ya tienes cuenta? <a class="form-link" href="/login" rel="noopener">Inicia sesión aquí</a></h3>
+            <div class="form-text">
+              <label for="InputUsername">Username</label>
+              <input type="text" class="form-control" required id="InputUsername">
+            </div>
+            <div class="form-text">
+              <label for="InputEmail">Correo electrónico </label>
+              <input type="email" class="form-control" required id="InputEmail" placeholder="correo@ejemplo.com">
+            </div>
+            <div class="form-text">
+              <label for="InputPassword">Contraseña</label>
+              <input type="password" class="form-control" required id="InputPassword">
+            </div>
+            <div class="form-text">
+              <label for="InputConfirmPassword">Confirme la contraseña</label>
+              <input type="password" class="form-control" required id="InputConfirmPassword">
+            </div>
+            <div class="form-text">
+              <label for="FechaNac">Fecha de Nacimiento</label>
+              <input class="form-control" required style="alignment: left" id="FechaNac" type="date" name="FechaNac"/>
+            </div>
+          </form>
+          <b-button class="register-btn text" text to="/sign-up-complete">
+            REGISTRARSE
+          </b-button>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "SignUp"
-    }
+export default {
+  name: "SignUp"
+}
 </script>
 
 <style lang="scss">
-    .SignUp {
-        height: 100vh;
-        display: grid;
-        background-image: url("../assets/SimpleBG.png");
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-        background-size: auto;
-    }
+.SignUp {
+  height: 100vh;
+  display: grid;
+  background-image: url("../assets/SimpleBG.png");
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: auto;
+}
 
-    .registrate-header{
-      text-align: left;
-      font-size: 1.7vw;
-      font-weight: 700;
-      color: #202020;
-    }
+.registrate-header {
+  text-align: left;
+  font-size: 1.7vw;
+  font-weight: 700;
+  color: #202020;
+}
 
-    .form-text {
-        text-align: left;
-        font-size: 14px;
-        font-weight: 500;
-        color: #707070;
-        width: 85%;
-        margin-right: 25%;
-    }
+.form-text {
+  text-align: left;
+  font-size: 1vw;
+  font-weight: 500;
+  color: #707070;
+  width: 85%;
+  margin-right: 25%;
+}
 
-    .form-text:focus-within {
-        text-align: left;
-        font-size: 14px;
-        font-weight: 500;
-        color: #202020;
-    }
+.form-text:focus-within {
+  text-align: left;
+  font-size: 1vw;
+  font-weight: 500;
+  color: #202020;
+}
 
-    .form-link{
-        font-size: 14px;
-        font-weight: 500;
-        position: relative;
-        left: 4%;
-        text-decoration: underline;
-    }
+.form-link {
+  font-size: 1vw;
+  font-weight: 500;
+  position: relative;
+  left: 4%;
+  text-decoration: underline;
+  color: #4D56D1;
+}
 
-    .form-control{
-        text-align: left;
-        font-size: 16px;
-        font-weight: 500;
-        color: #202020;
-    }
+.form-control {
+  text-align: left;
+  font-size: 1vw;
+  font-weight: 500;
+  color: #202020;
+}
 
-    .text {
-        text-align: left;
-        font-size: 14px;
-        font-weight: 500;
-        color: #202020;
-    }
+.text {
+  text-align: left;
+  font-size: 1vw;
+  font-weight: 500;
+  color: #202020;
+}
 
-    .close-x{
-        width: 1vw;
-        height: auto;
-        position: relative;
-        left: 40%;
-        bottom: 6%;
-    }
+.close-x {
+  width: 1vw;
+  height: auto;
+  position: relative;
+  left: 40%;
+  bottom: 6%;
+}
 
-    .back{
-        background-color: white;
-        margin: auto;
-        width: 57vw;
-    }
+.back {
+  background-color: white;
+  margin: auto;
+  width: 57vw;
+}
 
-    .register-btn {
-      width: 25vw;
-      height: 5vh;
-      border-radius: 50px;
-      background: linear-gradient(90deg, #7591ff 0%, #4dd1be 100%);
-      font-size: 1vw;
-      display: flex;
-      justify-content: center;
-      align-self: center;
-      line-height: (9 / 20);
-      font-weight: 500;
-      color: #fff;
-      border-width: 0;
-      margin-top: 5vh;
-      .text{
-        font-size: 1vw;
-        font-weight: 500;
-        margin-left: -0.5vw;
-        margin-top: 1vh;
-        color: #fff;
-      }
-    }
+.register-btn {
+  width: 25vw;
+  height: 5vh;
+  border-radius: 50px;
+  background: linear-gradient(90deg, #7591ff 0%, #4dd1be 100%);
+  font-size: 1vw;
+  line-height: (9 / 20);
+  font-weight: 500;
+  color: #fff;
+  border-width: 0;
+  margin-top: 5vh;
+  display: grid;
+  place-items: center;
+}
 
-    .login-form > * {
-        margin-left: 6.5%;
-        margin-bottom: 5%;
-    }
-    #InputUsername, #InputPassword, #InputEmail, #InputConfirmPassword, #FechaNac{
-      width: 24vw;
-      margin-left: 0;
-      border-top: transparent;
-      border-left: transparent;
-      border-right: transparent;
-      border-radius: 0;
-      border-width: 0.2vw;
-    }
-    #InputUsername:focus, #InputPassword:focus, #InputEmail:focus, #InputConfirmPassword:focus, #FechaNac:focus{
-      border-color: #4D56D1;
-      outline: none 0 !important;
-      box-shadow: none;
-      -moz-box-shadow: none;
-      -webkit-box-shadow: none;
-    }
+.login-form>* {
+  margin-left: 2.3vw;
+  margin-bottom: 4vh;
+}
+
+#InputUsername,
+#InputPassword,
+#InputEmail,
+#InputConfirmPassword,
+#FechaNac {
+  transition: all .5s ease;
+  width: 24vw;
+  margin-left: 0;
+  border-top: transparent;
+  border-left: transparent;
+  border-right: transparent;
+  border-radius: 0;
+  border-width: 0.2vw;
+}
+
+#InputUsername:focus,
+#InputPassword:focus,
+#InputEmail:focus,
+#InputConfirmPassword:focus,
+#FechaNac:focus {
+  border-color: #4D56D1;
+  outline: none 0 !important;
+  box-shadow: none;
+  -moz-box-shadow: none;
+  -webkit-box-shadow: none;
+}
 </style>
